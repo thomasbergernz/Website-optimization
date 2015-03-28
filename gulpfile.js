b4115fcbd86f67766cbc0905645bea9de23b07af
Thomas Berger <thomasbe@gmail.com>
2015-03-29 10:17:50 +1300

    Initial images optimised, updatePositions performance optimised; GulpJS used to minify CSS and JS;

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
+var gzip = require('gulp-gzip');
+var minifyHTML = require('gulp-minify-html');
+ 
+gulp.task('compress', ['scripts'], function() {
+    return gulp.src('js/*.js')
+    .pipe(gzip())
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('default', function() {
+  return gulp.src('css/*.css')
+  .pipe(minify())
+  .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('minify-HTML', function() {
+	var opts = {
+	conditionals: true,
+	spare:true
+  	};
+  return gulp.src('*.html')
+  .pipe(minifyHTML(opts))
+  .pipe(gulp.dest('dist/html'));
+});
+
+gulp.task('scripts', ['minihtml'], ['default'], function() {
+  return gulp.src('js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'));
+});
